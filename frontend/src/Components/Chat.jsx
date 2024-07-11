923dc95509b0dddc1a229407b3d379e0fef44e9b
Samed Dallaa <samadodallaa@gmail.com>
2024-07-11 14:11:04 +0100

    added new message form

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -1,20 +1,24 @@
 import axios from "axios";
 import React, { useContext, useEffect, useRef, useState } from "react";
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Col, Image, Form, InputGroup, Button } from "react-bootstrap";
 import AuthContext from "../Contexts/AuthContext";
 import Lottie from "lottie-react";
 import loadingAnimation from "../assets/animations/loadingAnimation.json";
 import UtilsContext from "../Contexts/UtilsContext";
 import { Link } from "react-router-dom";
 import chatBg from "../assets/img/chatBg.jpg";
+import { FaImage, FaPaperclip, FaPaperPlane } from "react-icons/fa";
 const Chat = ({ chat }) => {
   const tokens = JSON.parse(localStorage.getItem("tokens"));
   const { user } = useContext(AuthContext);
   const { isImage } = useContext(UtilsContext);
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(true);
-  const messagesEndRef = useRef(null);
   const [contact, setContact] = useState({});
+  const [message, setMessage] = useState({ body: "", attachment: null });
+  const messagesEndRef = useRef(null);
+  const imageRef = useRef(null);
+  const fileRef = useRef(null);
 
   useEffect(() => {
     async function fetchMessages() {
@@ -51,6 +55,33 @@ const Chat = ({ chat }) => {
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
+  const handleChange = (e) => {
+    if (e.target.name === "body") {
+      setMessage({ ...message, body: e.target.value });
+    } else if (e.target.name === "image" || e.target.name === "file") {
+      setMessage({ ...message, body: e.target.files[0] });
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const SEND_MESSAGE_ENDPOINT = `http://localhost:8000/api/chats/users/${contact.id}/send`;
+      const response = await axios.post(SEND_MESSAGE_ENDPOINT, message, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${tokens.access}`,
+        },
+      });
+      const data = response.data;
+      if (response.status === 201) {
+        console.log(data);
+      } else {
+        console.log("something went wrong");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       {loading ? (
@@ -144,6 +175,64 @@ const Chat = ({ chat }) => {
           <div ref={messagesEndRef}></div>
         </>
       )}
+      <div
+        className="position-sticky bottom-0 py-1"
+        style={{ backgroundColor: "aliceblue" }}
+      >
+        <Form className="w-100 px-2" onSubmit={handleSubmit}>
+          <InputGroup className="m-0 p-0">
+            <Form.Control
+              type="text"
+              placeholder="Type in a message"
+              name="body"
+              onChange={handleChange}
+            />
+            <InputGroup.Text style={{ backgroundColor: "white" }}>
+              <Form.Control
+                type="file"
+                id="imageInput"
+                className="d-none"
+                accept="image/*"
+                ref={imageRef}
+                name="image"
+                onChange={handleChange}
+              />
+              <Form.Label
+                className="m-0 d-flex align-items-center"
+                htmlFor="imageInput"
+              >
+                <FaImage id="imageInput" />
+              </Form.Label>
+            </InputGroup.Text>
+            <InputGroup.Text style={{ backgroundColor: "white" }}>
+              <Form.Control
+                type="file"
+                id="fileInput"
+                className="d-none"
+                accept=".pdf, .txt, .doc, video/*"
+                ref={fileRef}
+                name="file"
+                onChange={handleChange}
+              />
+              <Form.Label
+                htmlFor="fileInput"
+                className="m-0 d-flex align-items-center"
+              >
+                <FaPaperclip />
+              </Form.Label>
+            </InputGroup.Text>
+            <InputGroup.Text style={{ backgroundColor: "white" }}>
+              <Button
+                variant="light"
+                className="p-0 d-flex align-items-center"
+                type="submit"
+              >
+                <FaPaperPlane />
+              </Button>
+            </InputGroup.Text>
+          </InputGroup>
+        </Form>
+      </div>
     </div>
   );
 };
