d82da19cffc279e88122f0e15d0dc364ef4680e5
Samed Dallaa <samadodallaa@gmail.com>
2024-07-14 17:16:22 +0100

    channels finally work

--- backend/conext/chat/consumers.py
+++ backend/conext/chat/consumers.py
@@ -9,11 +9,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.chat_id = self.scope["url_route"]["kwargs"]["chat_id"]
         self.chat_group_name = f"chat_{self.chat_id}"
-        await self.channel_layer.group_add(self.chat_id, self.channel_name)
+        await self.channel_layer.group_add(self.chat_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.chat_id, self.channel_name)
+        await self.channel_layer.group_discard(self.chat_group_name, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -24,18 +24,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
         attachment = text_data_json.get("attachment", None)
         post = text_data_json.get("post", None)
 
-        sender = await User.objects.filter(pk=sender).first()
-        receiver = await User.objects.filter(pk=receiver).first()
-        chat = await Chat.objects.filter(pk=self.chat_id).first()
-        post = await Post.objects.filter(pk=post).first()
+        # sender = await User.objects.filter(pk=sender).first()
+        # receiver = await User.objects.filter(pk=receiver).first()
+        # chat = await Chat.objects.filter(pk=self.chat_id).first()
+        # post = await Post.objects.filter(pk=post).first()
 
-        new_message = await Message.objects.create(
-            chat=chat,
-            sender=sender,
-            receiver=receiver,
-            attachment=attachment,
-            post=post,
-        )
+        # new_message = await Message.objects.create(
+        #     chat=chat,
+        #     sender=sender,
+        #     receiver=receiver,
+        #     attachment=attachment,
+        #     post=post,
+        # )
 
         await self.channel_layer.group_send(
             self.chat_group_name,
@@ -49,31 +49,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
             },
         )
 
-    async def send_message(self, event):
+    async def chat_message(self, event):
         body = event["body"]
         sender = event["sender"]
         receiver = event["receiver"]
         attachment = event["attachment"]
         post = event["post"]
-
-        await self.send(
-            text_data=json.dumps(
-                {
-                    "body": body,
-                    "sender": sender,
-                    "receiver": receiver,
-                    "attachment": attachment,
-                    "post": post,
-                }
-            )
+        print(event)
+        await self.create_message(
+            data={
+                "chat_id": self.chat_id,
+                "body": body,
+                "sender": sender,
+                "receiver": receiver,
+                "attachment": attachment,
+                "post": post,
+            }
         )
 
     @database_sync_to_async
     def create_message(self, data):
-        get_chat_by_id = Chat.objects.filter(id=data["chat_id"]).first()
+        print("im running")
+        get_chat_by_id = Chat.objects.filter(id=self.chat_id).first()
         get_sender_by_id = User.objects.filter(id=data["sender"]).first()
         get_receiver_by_id = User.objects.filter(id=data["receiver"]).first()
-        get_post_by_id = User.objects.filter(id=data["post"]).first()
+        get_post_by_id = Post.objects.filter(id=data["post"]).first()
         new_message = Message.objects.create(
             chat=get_chat_by_id,
             sender=get_sender_by_id,
